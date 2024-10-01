a554e8b95e9ab231c4e3fb57ba1dccb24e452fda
Prashant dubey <pacific.prashantdubey@gmail.com>
2024-10-01 08:48:35 +0530

    Create MyCalculator.java

---
+++ myCalculator/src/main/MyCalculator.java
@@ -0,0 +1,20 @@
+public class MyCalculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+}
